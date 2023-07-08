5a654a12e0085900985e33e6da081c3bc85bff30
20120511 <110280412+khoamajik1752@users.noreply.github.com>
2023-07-08 22:40:27 +0700

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from app import views
-
+from django.contrib import admin
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", views.home, name="home"),
     path("new-cart", views.newcart, name="reload-cart"),
     path('calc-totalsum',views.calcTotalSum,name="calc-totalsum"),
@@ -11,5 +12,6 @@ urlpatterns = [
     path('add-to-cart',views.addToCart, name='add-to-cart'),
     path('set-quantity', views.setQuantity, name='set-quantity')
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

39aa33c3e3d9e2e9cc4dadd7e1f91b52991bcf71
20120511 <110280412+khoamajik1752@users.noreply.github.com>
2023-07-09 00:40:11 +0700

    edit script

--- static/scripts/cart.js
+++ static/scripts/cart.js
@@ -1,59 +1,87 @@
 
-function checkEmty(){
-    Amount= $('.App_title_amount').text()
-    if(Amount=="$0"){
-        $('.EmptyBlock').show()
-    }else{
-     
+$(document).ready(function () {
+    let sum = calcSum();
 
-        $('.EmptyBlock').hide()
+    $('.App_title_amount').text('$' + parseFloat(sum).toFixed(2))
+    function checkEmty() {
+        Amount = $('.App_title_amount').text().trim().split("$")[1]
+        console.log("amount"+parseFloat(Amount))
+        if (parseFloat(Amount) == 0) {
+            $('.EmptyBlock').show()
+        } else {
+
+
+            $('.EmptyBlock').hide()
+        }
     }
-}
-$(document).ready(function(){
+
+    function calcSum() {
+        var cartItems = document.querySelectorAll('.cart_items')
+        var totalsum = 0;
+        cartItems.forEach(function (e) {
+            var id = e.id;
+            console.log("id:"+id)
+            var price = parseFloat($(`#${id} .Cart_itemPrice`).text().split("$")[1])
+            var quantity = parseFloat($(`#${id} .Cart_itemCountNumber`).text())
+            totalsum = parseFloat(totalsum) + price * quantity;
+            totalsum = parseFloat(totalsum).toFixed(2)
+        });
+
+        return totalsum
+    }
+
     $('.addItem').off()
-    $('.addItem').click(async function(e){
+    $('.addItem').click(async function (e) {
         e.preventDefault()
         console.log('++')
-        id=$(this).attr('pro-id')
-        quantity= $(`#p${id} .Cart_itemCountNumber`).text()
+        var id = $(this).attr('pro-id')
+        var quantity = $(`#p${id} .Cart_itemCountNumber`).text()
         console.log(quantity)
-        quantity=parseInt(quantity) +1;
+        quantity = parseInt(quantity) + 1;
+        console.log("quantity" + quantity)
         $(`#p${id} .Cart_itemCountNumber`).text(quantity)
-        console.log(quantity)
+
+        let sum = calcSum();
+        $('.App_title_amount').text('$' + sum)
+        checkEmty()
+        console.log("quantity" + quantity)
         await $.ajax({
             type: "get",
             url: "/set-quantity",
             data: {
-                id:id,
-                quantity:quantity
+                id: id,
+                quantity: quantity
             },
             success: function (response) {
-                
+                console.log("success")
             }
         });
-        await $.ajax({
-            type: "get",
-            url: "/calc-totalsum",
-            success: function (response) {
-                console.log(response['totalsum'])
-                $('.App_title_amount').text('$'+response['totalsum'])
-                checkEmty()
-            }
-        });
-  
-    
+
+        // await $.ajax({
+        //     type: "get",
+        //     url: "/calc-totalsum",
+        //     success: function (response) {
+        //         console.log(response['totalsum'])
+        //         $('.App_title_amount').text('$'+response['totalsum'])
+        //         checkEmty()
+        //     }
+        // });
+
+
     });
-    
-    $('.delItem').click(async function(e){
+    $('.delItem').off()
+    $('.delItem').click(async function (e) {
         e.preventDefault()
         console.log("script")
         console.log('++')
-        id=$(this).attr('pro-id')
-        quantity= $(`#p${id} .Cart_itemCountNumber`).text()
-        quantity=parseInt(quantity) -1;
+        var id = $(this).attr('pro-id')
+        var quantity = $(`#p${id} .Cart_itemCountNumber`).text()
+        quantity = parseInt(quantity) - 1;
         $(`#p${id} .Cart_itemCountNumber`).text(quantity)
-        console.log(quantity)
-        if(quantity<=0){
+        let sum = calcSum();
+        $('.App_title_amount').text('$' + sum)
+        checkEmty()
+        if (quantity <= 0) {
             $(`[key=${id}] .temp_item_add_button`).show()
             $(`[key=${id}] .temp_add_button`).hide()
             $(`[key=${id}] .item_add_button_inactive`).hide()
@@ -63,59 +91,63 @@ $(document).ready(function(){
             type: "get",
             url: "/set-quantity",
             data: {
-                id:id,
-                quantity:quantity
+                id: id,
+                quantity: quantity
             },
             success: function (response) {
-    
+
             }
         });
-        await $.ajax({
-            type: "get",
-            url: "/calc-totalsum",
-            success: function (response) {
-                console.log(response['totalsum'])
-                $('.App_title_amount').text('$'+response['totalsum'])
-                checkEmty()
-            }
-        });
+        // await $.ajax({
+        //     type: "get",
+        //     url: "/calc-totalsum",
+        //     success: function (response) {
+        //         console.log(response['totalsum'])
+        //         $('.App_title_amount').text('$'+response['totalsum'])
+        //         checkEmty()
+        //     }
+        // });
 
     });
-    
-    $('.Cart_itemRemove').click(async function(e){
+    $('.Cart_itemRemove').off()
+    $('.Cart_itemRemove').click(async function (e) {
         e.preventDefault()
         console.log('+')
-        id=$(this).attr('pro-id')
-        quantity=0
+        var id = $(this).attr('pro-id')
+        var quantity = 0
         $(`#p${id} .Cart_itemCountNumber`).text(quantity)
         console.log(quantity)
-        
-        if(quantity<=0){
+
+        if (quantity <= 0) {
             $(`[key=${id}] .temp_item_add_button`).show()
             $(`[key=${id}] .temp_add_button`).hide()
             $(`[key=${id}] .item_add_button_inactive`).hide()
             $(`#p${id}`).remove()
         }
+        let sum = calcSum();
+        $('.App_title_amount').text('$' + sum)
+        checkEmty()
+        console.log("quantity"+ quantity)
         await $.ajax({
             type: "get",
             url: "/set-quantity",
             data: {
-                id:id,
-                quantity:quantity
+                id: id,
+                quantity: quantity
             },
             success: function (response) {
-             
+
             }
         });
-        await $.ajax({
-            type: "get",
-            url: "/calc-totalsum",
-            success: function (response) {
-                console.log(response['totalsum'])
-                $('.App_title_amount').text('$'+response['totalsum'])
-                checkEmty()
-            }
-        });
+        // await $.ajax({
+        //     type: "get",
+        //     url: "/calc-totalsum",
+        //     success: function (response) {
+        //         console.log(response['totalsum'])
+        //         $('.App_title_amount').text('$'+response['totalsum'])
+        //         checkEmty()
+        //     }
+        // });
     });
- 
+
 })
